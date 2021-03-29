a27f521c6b8cef4eb7b6de86e2de01d9e95d2783
MutualDEX <35112540+MutualDEX@users.noreply.github.com>
2021-03-28 22:38:15 -0400

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-import { ChainId, JSBI, Percent, Token, WETH } from '@uniswap/sdk'
+import { ChainId, JSBI, Percent, Token, WETH } from '@bigswap/v3-sdk'
 import { AbstractConnector } from '@web3-react/abstract-connector'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'

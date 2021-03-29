ba613811377764d3f5ac3b4554b9bbe027f648d5
MutualDEX <35112540+MutualDEX@users.noreply.github.com>
2021-03-29 19:53:43 -0400

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -274,8 +274,8 @@ const NETWORK_LABELS: { [chainId in ChainId]?: string } = {
   const { account, chainId } = useActiveWeb3React()
 
   let userEthBalance = useETHBalances(account ? [account] : [])?.[account ?? '']
-  // const [isDark] = useDarkModeManager()
-  const [darkMode, toggleDarkMode] = useDarkModeManager()
+  const [isDark] = useDarkModeManager()
+  const [darkMode] = useDarkModeManager()
 
   return (
     <HeaderFrame>
